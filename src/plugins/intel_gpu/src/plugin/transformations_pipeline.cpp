42daf1d3f14642b9309cdee8dfdd21cffb75c90d
Kim, Eddy <eddy.kim@intel.com>
2025-03-05 22:11:39 +0900

    variadic_split + multiply fusion and move glufusion up

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -1042,6 +1042,8 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         // This Validate is needed for proper data type propagation after applying IncreasePositionIdsPrecision pass
         manager.register_pass<ov::pass::Validate>();
 
+        manager.register_pass<ov::pass::GLUFusion>();
+
         float activations_scale_factor = config.get_activations_scale_factor();
 
         if (activations_scale_factor > 0.f && infer_precision == ov::element::f16) {
@@ -1144,7 +1146,6 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         }
         manager.register_pass<ov::intel_gpu::UnsqueezeBroadcastReshapeSDPAFusion>();
 
-        manager.register_pass<ov::pass::GLUFusion>();
         manager.register_pass<ov::intel_gpu::IndirectKVCache>();
 
         auto kv_cache_compression_dt = config.get_kv_cache_precision();
@@ -1165,6 +1166,8 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         manager.register_pass<ov::intel_gpu::SinkReshape>();
 
         manager.register_pass<ov::intel_gpu::SwapMulTranspose>();
+
+        manager.register_pass<ov::intel_gpu::VariadicSplitMulFusion>();
 
         if (device_info.supports_immad) {
             bool asymmetric_dyn_quant = GPU_DEBUG_VALUE_OR(config.get_asym_dynamic_quantization(), false);
