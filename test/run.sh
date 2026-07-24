3b1d2b15dc37e669fd16a6d753d57be8bb54132b
Kim, Eddy <eddy.kim@intel.com>
2026-07-24 23:18:03 +0900

    test dump

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU -lc "{\"ATTENTION_BACKEND\" : \"SDPA\"}" -m /mnt/cached_oses/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf llama-2-7b-chat.jsonl
 
-TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU -lc "{\"ATTENTION_BACKEND\" : \"SDPA\"}" -m /mnt/cached_oses/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/llama-3.1-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf /home/gta/work/frameworks.ai.openvino.llm.prompts/4096/llama-3-8b.jsonl
+TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU -lc "{\"ATTENTION_BACKEND\" : \"SDPA\"}" -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/llama-3.1-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf /home/gta/work/frameworks.ai.openvino.llm.prompts/4096/llama-3-8b.jsonl
 
 
 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU -lc "{\"ATTENTION_BACKEND\" : \"SDPA\"}" -m /mnt/cached_oses/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/gemma-2-9b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf /home/gta/work/frameworks.ai.openvino.llm.prompts/4096/gemma-2-9b-it.jsonl
