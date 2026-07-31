ec68b818ac6481eb9ffc5b0e9669d22beedfde6e
Kim, Eddy <eddy.kim@intel.com>
2026-07-31 19:26:16 +0900

    MAX_BARRIER_V_PREFETCH

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=i8 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -lc "{\"ATTENTION_BACKEND\" : \"SDPA\"}" -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-2-7b-chat.jsonl
 
-TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-2-7b-chat.jsonl
+SDPA_OCL_256GRF=1 SDPA_OCL_KQ_TILE_QUERIES=32 SDPA_OCL_KQ_PER_WG_KEYS=4 SDPA_OCL_KQ_PER_WG_QUERIES=2 SDPA_OCL_MAX_BARRIER_V_PREFETCH=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-2-7b-chat.jsonl
 
 
 
