e0166f0737ae457e03131a1afc5b7c8587d7ca61
Kim, Eddy <eddy.kim@intel.com>
2026-08-06 16:14:54 +0900

    token major key cache

--- test/run.sh
+++ test/run.sh
@@ -2,12 +2,14 @@ TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=i8 python ~/work/openvino.genai/tools/
 
 SDPA_OCL_256GRF=1 SDPA_OCL_KQ_TILE_QUERIES=32 SDPA_OCL_KQ_PER_WG_KEYS=4 SDPA_OCL_KQ_PER_WG_QUERIES=2 SDPA_OCL_MAX_BARRIER_V_PREFETCH=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-2-7b-chat.jsonl
 
-
+OV_GPU_PA_K_TOKEN_MAJOR=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-2-7b-chat.jsonl --cb_config "{\"enable_prefix_caching\":true, \"max_num_batched_tokens\":256}"
 
 
 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=i8 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -lc "{\"ATTENTION_BACKEND\" : \"SDPA\"}" -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/llama-3.1-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-3-8b.jsonl
 
 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/llama-3.1-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-3-8b.jsonl
+
+OV_GPU_PA_K_TOKEN_MAJOR=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW25_llm-optimum_2026.3.0-22208/llama-3.1-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/llama-3-8b.jsonl --cb_config "{\"enable_prefix_caching\":true, \"max_num_batched_tokens\":256}"
 
 
 
@@ -18,6 +20,8 @@ SDPA_OCL_256GRF=1 SDPA_OCL_KQ_TILE_QUERIES=32 SDPA_OCL_KQ_PER_WG_KEYS=8 SDPA_OCL
 
 
 
+OV_GPU_PA_K_TOKEN_MAJOR=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/gemma-2-9b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/gemma-2-9b-it.jsonl --cb_config "{\"enable_prefix_caching\":true, \"max_num_batched_tokens\":256}"
+
 
 
 
@@ -25,8 +29,7 @@ TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=i8 python ~/work/openvino.genai/tools/
 
 SDPA_OCL_256GRF=1 SDPA_OCL_KQ_TILE_QUERIES=32 SDPA_OCL_KQ_PER_WG_KEYS=8 SDPA_OCL_KQ_PER_WG_QUERIES=2 SDPA_OCL_V_F16_MULTIBLOCK=1 SDPA_OCL_MAX_BARRIER_V_PREFETCH=0 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/gemma-3-4b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/gemma-3-4b-it.jsonl
 
-
-
+OV_GPU_PA_K_TOKEN_MAJOR=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/gemma-3-4b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/gemma-3-4b-it.jsonl --cb_config "{\"enable_prefix_caching\":true, \"max_num_batched_tokens\":256}"
 
 
 
@@ -36,3 +39,4 @@ TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=i8 python ~/work/openvino.genai/tools/
 
 SDPA_OCL_256GRF=1 SDPA_OCL_KQ_TILE_QUERIES=32 SDPA_OCL_KQ_PER_WG_KEYS=8 SDPA_OCL_KQ_PER_WG_QUERIES=2 SDPA_OCL_V_F16_MULTIBLOCK=1 SDPA_OCL_MAX_BARRIER_V_PREFETCH=0 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/gemma-4-e2b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/gemma-4-e2b-it.jsonl
 
+OV_GPU_PA_K_TOKEN_MAJOR=1 TEST_USE_SDPA_OCL=1 OV_KV_CACHE_PRECISION=f16 python ~/work/openvino.genai/tools/llm_bench/benchmark.py -d GPU.1 -m /mnt/models/ov-share-13.sclab.intel.com/cv_bench_cache/WW29_llm-optimum_2026.3.0-22446-RC2/gemma-4-e2b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/ -n 3 -ic 256 -pf /home/shingyuk/work/frameworks.ai.openvino.llm.prompts/4096/gemma-4-e2b-it.jsonl --cb_config "{\"enable_prefix_caching\":true, \"max_num_batched_tokens\":256}"
